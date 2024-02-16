433e5c525688c201804e3eb5ba042061fd70b84e
majesnix <majesnix@electronicintelligence.de>
2024-02-16 18:22:22 +0100

    feat: create and resolve shorturls

--- src/pages/InputUrl.tsx
+++ src/pages/InputUrl.tsx
@@ -1,9 +1,20 @@
 import { styled } from "@macaron-css/solid";
 import "./InputUrl.css";
+import { Show, createSignal, onMount } from "solid-js";
+import { writeClipboard } from "@solid-primitives/clipboard";
 
 const Wrapper = styled("div", {
   base: {
     height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const InputWrapper = styled("div", {
+  base: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
@@ -17,11 +28,49 @@ const Input = styled("input", {
   },
 });
 
-const InputUrl = () => (
-  <Wrapper>
-    <Input placeholder="https://.."></Input>
-    <button class="shorten-button">Shorten</button>
-  </Wrapper>
-);
+const Short = styled("div", {
+  base: {
+    color: "white",
+    marginTop: "8px",
+  },
+});
+
+const InputUrl = () => {
+  const [url, setUrl] = createSignal("");
+  const [short, setShort] = createSignal("");
+
+  const shorten = async () => {
+    const { short } = (await fetch(import.meta.env.VITE_API, {
+      method: "POST",
+      body: JSON.stringify({ url: url() }),
+    }).then(async (response) => await response.json())) as { short: string };
+
+    setShort("https://" + import.meta.env.VITE_BASE + "/" + short);
+  };
+
+  onMount(() => {
+    const input = document.getElementById("input")! as HTMLInputElement;
+    input.value = "https://";
+    input.focus();
+  });
+
+  return (
+    <Wrapper>
+      <InputWrapper>
+        <Input
+          id="input"
+          placeholder="https://.."
+          onInput={(e) => setUrl(e.target.value)}
+        ></Input>
+        <button class="shorten-button" onClick={shorten}>
+          Shorten
+        </button>
+      </InputWrapper>
+      <Show when={short()}>
+        <Short onClick={() => writeClipboard(short())}>{short()}</Short>
+      </Show>
+    </Wrapper>
+  );
+};
 
 export default InputUrl;
--- src/pages/ResolveUrl.tsx
+++ src/pages/ResolveUrl.tsx
@@ -1,3 +1,26 @@
-const ResolveUrl = () => <div></div>
+import { Show, createSignal, onMount } from "solid-js";
+
+const ResolveUrl = () => {
+  const [notFound, setNotFound] = createSignal(false);
+  onMount(async () => {
+    const url = await fetch(
+      `${import.meta.env.VITE_API}/${location.pathname.replace("/", "")}`
+    ).then(async (response) => {
+      if (response.ok) {
+        return (await response.json()).url;
+      }
+    });
+
+    if (url) {
+      if (!url.includes("http") || !url.includes("https")) {
+        window.location.href = "https://" + url;
+      } else {
+        window.location.href = url;
+      }
+    }
+  });
+
+  return <Show when={notFound}>NOT FOUND</Show>;
+};
 
 export default ResolveUrl;
